06f63fb8d52ff20caa23049b82d378b7e9ea0e01
Ian Mota <ianmotabr@gmail.com>
2022-08-09 00:46:45 -0300

    app-ok-00

--- revisionManagment.py
+++ revisionManagment.py
@@ -1,51 +1,56 @@
 import shutil
 import os
 
-def FileMovimentation(arquivos,destino):
+class FileMovimentation():
+    
+    def __init__(self,arquivo:str,destino:str) -> None:
+        self.arquivo = arquivo
+        self.destino = destino
 
-    for i in arquivos:
-        
-        os.chdir(destino)
-        diretorio = os.listdir(destino)
-        nomeArquivos = os.path.basename(i)
-        
-        quebra01 = nomeArquivos.split("-")
+    def NameSplit(self):
+        quebra01 = self.FileName().split("-")
         quebra02 = quebra01[-1].split(".")
-        
-        versao02 = quebra02[0]
-        
-        if("R" in versao02):
-            versao02 =versao02.split("R")[1]
+        return quebra02[0]
 
-        if(int(versao02)<=10):
-            
-            versao_ = int(versao02)-1
-            versaoAnterior = f"0{versao_}"
-            revisaoAntiga = nomeArquivos.replace(versao02,versaoAnterior)
+    def VerPrefix(self):
+        if "R" in self.NameSplit():
+            return(self.NameSplit().split("R")[1])
+    
+    def RevisionWithoutPrefix(self):
         
-        if(int(versao02)>10):
+        if(int(self.NameSplit())<=10):
+            versao_ = int(self.NameSplit())-1
+            versaoAnterior = f"0{versao_}."
+            revisaoAntiga = self.FileName().replace(f"{self.NameSplit()}.",versaoAnterior)
             
-            versao_ = int(versao02)-1
-            versaoAnterior = f"{versao_}"
-            revisaoAntiga = nomeArquivos.replace(versao02,versaoAnterior)
-        if (revisaoAntiga in diretorio):
-            arquivo = os.path.abspath(diretorio[diretorio.index(revisaoAntiga)])
-        else:
-            print("ERRO")
-            continue
+        elif(int(self.versao02)>10):
+            versao_ = int(self.NameSplit())-1
+            versaoAnterior = f"{versao_}."
+            revisaoAntiga = self.FileName().replace(f"{self.NameSplit()}.",versaoAnterior)
+
+        return revisaoAntiga
+    
+    def RevisionPrefix(self):
+        
+        if(int(self.VerPrefix())<=10):
+            versao_ = int(self.VerPrefix())-1
+            versaoAnterior = f"0{versao_}."
+            revisaoAntiga = self.FileName().replace(f"{self.VerPrefix()}.",versaoAnterior)
             
-        if "Antigo" in diretorio:
-            novoDiretorio = os.path.abspath(f'{destino}/Antigo')
-            
-        elif "Antigos" in diretorio:
-            novoDiretorio = os.path.abspath(f'{destino}/Antigos')
-            
-        else:
-            novoDiretorio = os.path.abspath(destino+"/Antigos")
-            os.mkdir(novoDiretorio)
-            
-        shutil.move(arquivo,novoDiretorio)
-        shutil.move(i,destino)
+        elif(int(self.VerPrefix())>10):
+            versao_ = int(self.VerPrefix())-1
+            versaoAnterior = f"{versao_}."
+            revisaoAntiga = self.FileName().replace(f"{self.VerPrefix()}.",versaoAnterior)
+
+        return revisaoAntiga
+        
+    def FileName(self):
+        return(os.path.basename(self.arquivo))
+    
+    def GetFilesInDir(self):
+        destino = self.destino
+        return(os.listdir(destino))
+        
 
     
             
